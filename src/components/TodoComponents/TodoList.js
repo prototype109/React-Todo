4bd3fe2786c55389898de82c332854e1a411b192
Brandon Nichols <brandonaronnichols@gamil.com>
2019-09-03 22:54:42 -0500

    added basic todo list showing items, no add or delete functionality yet

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,28 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+
+class TodoList extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            list : [
+                'first',
+                'second',
+                'third'
+            ]
+        }
+    }
+
+    render(){
+        return(
+            <ul>
+                {this.state.list.map( (item, index) => (
+                    <li key={index}>{item}</li>
+        ))}
+            </ul>
+        )
+    }
+}
+
+export default TodoList;
